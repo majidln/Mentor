ad4fe1b7e3698daa690b1c5454fa4c79d889e6d2
Majid Lotfinia <majidlotfinia12@gmail.com>
2021-06-14 11:15:24 +0430

    add android splash with sample icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {ActivityIndicator} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
+
 
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigation from './navigation/index';
 import {store, persistor} from './store/index';
-import AppTheme from './services/theme'
+import AppTheme from './services/theme';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate
